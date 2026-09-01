dfeef26ebed16394b607f59f8a20e17dacd81a2e
pooria1644 <p.piroozi7777@gmail.com>
2026-09-02 01:31:14 +0330

    Bug fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class nestedlist(list):
-    def __getitem__(self, i: int|tuple[int, ...]):
+    def __getitem__(self, i: int|slice|tuple[int, ...]):
         try:
             res = self
             for j in i:
