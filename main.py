9e9b7ab7aa6814b5ba4a6043d029d5b194e92db0
pooria1644 <p.piroozi7777@gmail.com>
2026-09-02 01:07:52 +0330

    Initial release

---
+++ main.py
@@ -0,0 +1,12 @@
+class nestedlist(list):
+    def __getitem__(self, i: int|tuple[int, ...]):
+        try:
+            res = self
+            for j in i:
+                res = res[j]
+            return res
+        except:
+            return list(self).__getitem__(i)
+
+
+nl = nestedlist
